29c0c7a961b3bb17187db3d52e65685954ff48b1
Dmitry Yan <rocketman27@inbox.ru>
2019-06-22 22:59:45 +0300

    Добавлены параметры в конфигурационный файл

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+  // This is configuration file. Be fuckin' accurate!
+
+  # Date & Time
+  define(ESN_TIMEZONE_SET, 'Europe/Moscow');
+
+  # MySQL
+  define(ESN_MYSQL_UNAME, 'root');
+  define(ESN_MYSQL_PWRD, '');
+  define(ESN_MYSQL_SERVER, '127.0.0.1');
+  define(ESN_MYSQL_DB, '');
+
+  # Site info
+  define(ESN_SITE_NAME, 'Sample Site');
+  define(ESN_SITE_SLOGAN, 'Warning: very sample lamas..')
+  define(ESN_SITE_DOMAIN, 'SampleDomain.com');
+  define(ESN_SITE_COPYRIGHT, '&copy; ' . date("Y") . ' ' . ESN_SITE_DOMAIN);
+
+  # Booleans
+  define(ESN_BOOL_SHOW_SLOGAN, TRUE);
