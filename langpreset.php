c7ae45ed6104041e5e6999b8c21d173577f4d69d
Dmitry Yan <rocketman27@inbox.ru>
2019-06-26 20:17:55 +0400

    Опять что-то с языками

--- langpreset.php
+++ langpreset.php
@@ -1,13 +1,25 @@
 <?php
 
   $default_lang_pkgs = array(
-    'engine-messages-en',
+    #'engine-messages-en',
     'engine-messages-ru'
     // todo: create more translations
   );
 
   for ($i=0; $i2=0; $i < count(ESN_USING_PKGS); $i2 < count($default_lang_pkgs); $i++; $i2++) {
       if (ESN_USING_PKGS[$i] == $default_lang_pkgs[$i2]) {
-
+        $_Lang = array(
+          'cct_err' => 'Connect error',
+          'con_establ' => 'Connection Established',
+          'err' => 'Error',
+          'y' => 'Yes',
+          'n' => 'No',
+          'cfg' => 'Configuration',
+          'main' => 'Main',
+          'del' => 'Delete',
+          'authrz' => 'Authorize',
+          'install_pkg' => 'Install Package',
+          'pages' => 'Pages'
+        );
       }
   }
