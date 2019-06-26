8fe81410f055ebc0471fff9ab33439cdf28419c2
Dmitry Yan <rocketman27@inbox.ru>
2019-06-26 17:17:45 +0400

    Думаю над проблемой с лангпаками

--- include.php
+++ include.php
@@ -6,3 +6,5 @@
   for ($i=0; $i < count(ESN_USING_PKGS); $i++) {
       include './pkg/' . ESN_USING_PKGS[$i] . '/init.php';
   }
+
+  include 'langpreset.php';
---
+++ langpreset.php
@@ -0,0 +1,13 @@
+<?php
+
+  $default_lang_pkgs = array(
+    'engine-messages-en',
+    'engine-messages-ru'
+    // todo: create more translations
+  );
+
+  for ($i=0; $i2=0; $i < count(ESN_USING_PKGS); $i2 < count($default_lang_pkgs); $i++; $i2++) {
+      if (ESN_USING_PKGS[$i] == $default_lang_pkgs[$i2]) {
+
+      }
+  }
